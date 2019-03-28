61d99231fa28cd866d80d6b20167e223a5d1c89b
Kameron Kenny <kameron.kenny@salesforce.com>
2019-03-28 09:40:47 -0400

    1. Fix style 2. add Notes array 3. clear the input variable for older versions of BASH

--- timer.sh
+++ timer.sh
@@ -5,42 +5,44 @@ function get_time() {
   echo ${date_time}
 }
 
-segments=(Introduction Video Response "Agile Feedback")
+segments=(Introduction Video Response "Agile Feedback Review")
 lengths=(5 4 3 5)
+notes=("Thank you..." "Set audio input\nPlay Video" "*Reset audio input\nAsk for response" "Feedback Deck")
 
-c=0
+c=-1
 
 while true; do
-	clear
-	echo
-	date	
-	echo -e "\n\n"
+  clear
+  echo
+  date
+  echo -e "\n\n"
 
-	if [[ "$c" -eq "0" ]]; then 
-		echo "Upcoming:"
-		for i in ${!segments[@]}; do
-			echo "${segments[$i]}, ${lengths[$i]} minute(s) "
-		done
-	else
-		echo "Current: ${segments[$c]}"
-		echo "Up Next: ${segments[$((c + 1))]}"
-	fi
+  if [[ "$c" -eq "-1" ]]; then
+    echo "Upcoming:"
+    for i in ${!segments[@]}; do
+      echo "${segments[$i]}, ${lengths[$i]} minute(s) "
+    done
+  else
+    echo "Current: ${segments[$c]}"
+    echo "Up Next: ${segments[$((c + 1))]}"
+    echo -e "\nNotes:\n${notes[$c]}"
+  fi
 
   read -rsn1 -t1 input
-  
+
   case $input in
-       s)      
-         start_timer
-         ;;
-       n)      
-         c=$((c + 1))
-         ;;
-       b)      
-         c=$((c - 1))
-         ;;
-       q)      
-         exit 0
-         ;;
+    s)
+      start_timer
+      ;;
+    n)
+      c=$((c + 1))
+      ;;
+    b)
+      c=$((c - 1))
+      ;;
+    q)
+      exit 0
+      ;;
   esac
 
   if [ "$input" = "a" ]; then
@@ -48,5 +50,8 @@ while true; do
   fi
 
 #  sleep 0.11
+
+  # Clear input for older versions of bash
+  input=''
 
 done
