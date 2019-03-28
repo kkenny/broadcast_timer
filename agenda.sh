896d49c0e5753f0c53a933cde25769e8d011b077
Kameron Kenny <kameron.kenny@salesforce.com>
2019-03-28 14:19:09 -0400

    read agenda from agenda.sh

---
+++ agenda.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+segments=("Introduction" "Video" "Response" "Agile Feedback Review" "else")
+lengths=(5 4 3 5 1)
+notes=("Thank you..." "Set audio input\nPlay Video" "*Reset audio input\nAsk for response" "Feedback Deck")
